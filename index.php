02f7f2020475f3b024bbec417b02751dd1361bb7
viniciusbeckerbernardini <vbkmma@gmail.com>
2019-02-04 13:59:31 -0200

    Adicionei as operações de crud

--- index.php
+++ index.php
@@ -1,90 +1,158 @@
-<?php 
+ <?php 
 
-session_start();
+ session_start();
 
-require_once("vendor/autoload.php");
+ require_once("vendor/autoload.php");
 
-use \Slim\Slim;
-use \Hcode\Page;
-use \Hcode\PageAdmin;
-use \Hcode\Model\User;
+ use \Slim\Slim;
+ use \Hcode\Page;
+ use \Hcode\PageAdmin;
+ use \Hcode\Model\User;
 
-$app = new Slim();
+ $app = new Slim();
 
-$app->config('debug', true);
+ $app->config('debug', true);
 
-$app->get('/', function() {
+ $app->get('/', function() {
+
+ 	$page = new Page();
+
+ 	$page->setTpl("index");
+
+ });
+
+ $app->get('/admin', function(){
+
+ 	User::verifyLogin();
+
+ 	$page = new PageAdmin();
+
+ 	$page->setTpl("index");
+ });
+
+
+ $app->get('/admin/login', function(){
+
+ 	$page = new PageAdmin([
+ 		"header"=>false,
+ 		"footer"=>false
+ 	]);
+
+ 	$page->setTpl("login");
+ });
+
+ $app->post('/admin/login',function(){
+
+ 	User::login(filter_input(INPUT_POST, 'login'),filter_input(INPUT_POST, 'password'));
+
+ 	header('Location: /admin');
+
+ 	exit;
+ });
+
+ $app->get('/admin/logout',function(){
+ 	User::logout();
+ });
+
+ $app->get('/admin/users',function(){
+
+ 	User::verifyLogin();
+
+ 	$users = User::listAll();
+
+ 	$page = new PageAdmin();
+
+ 	$page->setTpl("users",["users"=>$users]);
+
+ });
+
+ $app->get('/admin/users/create',function(){
+
+ 	User::verifyLogin();
+
+ 	$page = new PageAdmin();
+
+ 	$page->setTpl("users-create");
+
+ });
+
+ $app->post('/admin/users/create',function(){
+
+ 	User::verifyLogin();
+
+ 	$user = new User();
+
+ 	$_POST['inadmin'] = (isset($_POST['inadmin']))? 1 :  0;
+
+ 	$user->setData($_POST);
+
+ 	$user->save();
+
+ 	header('Location: /admin/users');
+
+ 	exit;
+
+
+ });
+
+ $app->get('/admin/users/:iduser/delete',function($iduser){
+
+ 	User::verifyLogin();
+
+ 	$user = new User();
+
+ 	$user->get((int)$iduser);
+
+ 	$user->delete();
+
+ 	header("Location: /admin/users");
+
+ 	exit;
+ });
+
+ $app->get('/admin/users/:iduser',function($iduser){
+
+ 	User::verifyLogin();
+
+ 	$user = new User();
+
+ 	$user->get((int)$iduser);
+
+ 	$_POST['inadmin'] = (isset($_POST['inadmin']))? 1 :  0;
+
+
+ 	$page = new PageAdmin();
+
+ 	$page->setTpl(
+ 		"users-update",
+ 		array(
+ 		"user"=>$user->getValues()
+ 		));
+
+ });
+
+ $app->post('/admin/users/:iduser', function($iduser){
+ 	
+ 	User::verifyLogin();
+
+ 	$_POST['inadmin'] = (isset($_POST['inadmin']))? 1 :  0;
 	
-	$page = new Page();
+	$user = new User();
 
-	$page->setTpl("index");
+ 	$user->get((int)$iduser);
 
-});
+ 	$user->setData($_POST);
 
-$app->get('/admin', function(){
+ 	$user->update();
 
-	User::verifyLogin();
+ 	header("Location: /admin/users");
+ 	exit;
 
-	$page = new PageAdmin();
-
-	$page->setTpl("index");
-});
+ });
 
 
-$app->get('/admin/login', function(){
-	
-	$page = new PageAdmin([
-		"header"=>false,
-		"footer"=>false
-	]);
-
-	$page->setTpl("login");
-});
-
-$app->post('/admin/login',function(){
-	
-	User::login(filter_input(INPUT_POST, 'login'),filter_input(INPUT_POST, 'password'));
-
-	header('Location: /admin');
-
-	exit;
-});
-
-$app->get('/admin/logout',function(){
-	User::logout();
-});
-
-$app->get('/admin/users',function(){
-
-	User::verifyLogin();
-
-	$page = new PageAdmin();
-
-	$page->setTpl("user");
-
-});
-
-$app->get('/admin/users/create',function(){
-	
-	User::verifyLogin();
-
-	$page = new PageAdmin();
-
-	$page->setTpl("user-create");
-
-});
-
-$app->get('/admin/users/:idUser',function($idUser){
-	
-	User::verifyLogin();
-
-	$page = new PageAdmin();
-
-	$page->setTpl("user-update");
-
-});
 
 
-$app->run();
+ $app->run();
 
-?>
+ ?>
--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -58,9 +58,75 @@ class User extends Model{
 		}
 	}
 
-	public static function logout(){
+	public static function logout()
+	{
 		$_SESSION[User::SESSION] = NULL;
 		header("Location: /admin/login");
 		exit;
+	}
+
+	public static function listAll()
+	{
+		$sql = new Sql();
+		return $sql->select("SELECT * FROM tb_users a  INNER JOIN tb_persons b USING(idperson) ORDER BY b.desperson");
+
+
+	}
+
+	public function save(){
+
+		$sql = new Sql();
+		try {
+			$results = $sql->select("CALL sp_users_save(:desperson, :deslogin, :despassword,:desemail, :nrphone, :inadmin)",
+				array(
+					":desperson"=>$this->getdesperson(),
+					":deslogin"=>$this->getdeslogin(),
+					":despassword"=>$this->getdespassword(),
+					":desemail"=>$this->getdesemail(),
+					":nrphone"=>$this->getnrphone(),
+					":inadmin"=>$this->getinadmin()
+				));
+			$this->setData($results[0]);
+		} catch (Exception $e) {
+			throw new \Exception($e->getMessage());	
+		}
+	}
+
+	public function get($iduser)
+	{
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_users a inner join tb_persons b USING(idperson) where a.iduser = :iduser", array(":iduser"=>$iduser));
+
+		$this->setData($results[0]);
+	}
+
+	public function update()
+	{
+
+		$sql = new Sql();
+		try {
+			$results = $sql->select("CALL sp_usersupdate_save(:iduser,:desperson, :deslogin, :despassword,:desemail, :nrphone, :inadmin)",
+				array(
+					":iduser"=>$this->getiduser(),
+					":desperson"=>$this->getdesperson(),
+					":deslogin"=>$this->getdeslogin(),
+					":despassword"=>$this->getdespassword(),
+					":desemail"=>$this->getdesemail(),
+					":nrphone"=>$this->getnrphone(),
+					":inadmin"=>$this->getinadmin()
+				));
+
+			$this->setData($results[0]);
+
+		} catch (Exception $e) {
+			throw new \Exception($e->getMessage());	
+		}
+	}
+
+	public function delete()
+	{
+		$sql = new Sql();
+		$sql->query("CALL sp_users_delete(:iduser)", array(":iduser"=>$this->getiduser()));
 	}
 }
