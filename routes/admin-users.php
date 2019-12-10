f957b98f221730bd000b2e5bbf3f52c6f757571d
viniciusbeckerbernardini <vbkmma@gmail.com>
2019-12-10 13:05:27 -0300

    Aula 133 concluída

--- routes/admin-users.php
+++ routes/admin-users.php
@@ -4,6 +4,47 @@ use \Hcode\Page;
 use \Hcode\PageAdmin;
 use \Hcode\Model\User;
 
+$app->post("/admin/users/:iduser/password", function($iduser){
+    User::verifyLogin(false);
+    if (!isset($_POST['despassword']) || $_POST['despassword']==='') {
+        User::setMsgError("Preencha a nova senha.");
+        header("Location: /admin/users/$iduser/password");
+        exit;
+    }
+    if (!isset($_POST['despassword-confirm']) || $_POST['despassword-confirm']==='') {
+        User::setMsgError("Preencha a confirmação da nova senha.");
+        header("Location: /admin/users/$iduser/password");
+        exit;
+    }
+    if ($_POST['despassword'] !== $_POST['despassword-confirm']) {
+        User::setMsgError("Confirme corretamente as senhas.");
+        header("Location: /admin/users/$iduser/password");
+        exit;
+    }
+    $user = new User();
+    $user->get((int)$iduser);
+    $user->setPassword(User::getPasswordHash($_POST['despassword']));
+    User::setMsg("Senha alterada com sucesso.");
+    header("Location: /admin/users/$iduser/password");
+    exit;
+});
+
+$app->get("/admin/users/password/:iduser/",function ( int $iduser){
+   User::verifyLogin();
+
+   $user = new User();
+
+   $user->get($iduser);
+
+   $page = new PageAdmin();
+
+   $page->setTpl("users-password",[
+       "user"=>$user->getValues(),
+       "msgError"=>User::getMsgError(),
+       "msgSuccess"=>User::getMsg()
+   ]);
+});
+
 $app->get('/admin/users',function(){
 	User::verifyLogin();
 
--- vendor/hcodebr/php-classes/src/Model/Address.php
+++ vendor/hcodebr/php-classes/src/Model/Address.php
@@ -43,11 +43,12 @@ class Address extends Model
     public function save(){
         $sql = new Sql();
 
-        $results = $sql->select("CALL sp_addresses_save(:idaddress, :idperson, :desaddress, :descomplement, :descity, :desstate, :descountry, :deszipcode, :desdistrict)",
+        $results = $sql->select("CALL sp_addresses_save(:idaddress, :idperson, :desaddress,:desnumber,:descomplement, :descity, :desstate, :descountry, :deszipcode, :desdistrict)",
             [
                 ":idaddress" => 0,
                 ":idperson"=>$this->getidperson(),
                 ":desaddress"=> utf8_encode($this->getdesaddress()),
+                ":desnumber"=>$this->getdesnumber(),
                 ":descomplement" =>utf8_encode($this->getdescomplement()),
                 ":descity"=>utf8_encode($this->getdescity()),
                 ":desstate"=>utf8_encode($this->getdesstate()),
